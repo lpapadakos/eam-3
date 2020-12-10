49818229f182e9ebac5ab6c0904e6f009f7b0072
Leonidas P. Papadakos <papadakospan@gmail.com>
2020-12-10 17:27:24 +0200

    Use php include for common page areas

---
+++ htdocs/404.php
@@ -0,0 +1,75 @@
+<!DOCTYPE HTML>
+<html>
+<head><meta name="viewport" content="width=device-width"/>
+<title>Salique Theme Multipurpose Responsive </title>
+<!-- STYLES & JQUERY
+================================================== -->
+<link rel="stylesheet" type="text/css" href="css/style.css"/>
+<link rel="stylesheet" type="text/css" href="css/icons.css"/>
+<link rel="stylesheet" type="text/css" href="css/skinblue.css"/><!-- change skin color here -->
+<link rel="stylesheet" type="text/css" href="css/responsive.css"/>
+<script src="js/jquery-1.9.0.min.js"></script><!-- scripts are at the bottom of the document -->
+</head>
+<body>
+<!-- TOP LOGO & MENU
+================================================== -->
+<?php include 'topnav.php'; ?>
+<!-- HEADER
+================================================== -->
+<div class="undermenuarea">
+	<div class="boxedshadow">
+	</div>
+	<div class="grid">
+		<div class="row">
+			<div class="c8">
+				<h1 class="titlehead">Oops...</h1>
+			</div>
+			<div class="c4">
+				<h1 class="titlehead rightareaheader"><i class="icon-map-marker"></i> Call Us Now 1-318-107-432</h1>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- CONTENT
+================================================== -->
+<div class="grid">
+		<div class="shadowundertop"></div>
+		<div class="row space-top space-bot">
+			<div class="c1">
+			</div>
+			<div class="c10" style="text-align:center;margin-top:90px;margin-bottom:190px;">
+				<h1>SORRY, PAGE NOT FOUND</h1>
+				<div class="pageline">
+				</div>
+				<p>
+					 We've recently updated our site and the page you requested cannot be found. <br/>If you tried to load a previously bookmarked page, or followed a link from another web site, it is possible that the URL has been moved.
+				</p>
+				<br/>
+				<div class="actionbutton">
+					<a href="index.html"><i class=" icon-link"></i> VISIT OUR HOMEPAGE</a>
+				</div>
+			</div>
+			<div class="c1">
+			</div>
+		</div>
+</div><!-- end grid -->
+
+<!-- FOOTER
+================================================== -->
+<?php include 'footer.php' ?>
+<!-- JAVASCRIPTS
+================================================== -->
+<!-- all -->
+<script src="js/modernizr-latest.js"></script>
+
+<!-- menu & scroll to top -->
+<script src="js/common.js"></script>
+
+<!-- twitter -->
+<script src="js/jquery.tweet.js"></script>
+
+<!-- cycle -->
+<script src="js/jquery.cycle.js"></script>
+
+</body>
+</html>
---
+++ htdocs/topnav.php
@@ -0,0 +1,55 @@
+<div class="grid">
+	<div class="row space-bot">
+		<!--Logo-->
+		<div class="c4">
+			<a href="index.html">
+				<img src="images/logo.png" class="logo" alt="">
+			</a>
+		</div>
+		<!--Menu-->
+		<div class="c8">
+			<nav id="topNav">
+			<ul id="responsivemenu">
+				<li class="active"><a href="index.html"><i class="icon-home homeicon"></i></a></li>
+				<li><a href="#">Templates</a>
+				<ul style="display: none;">
+
+					<li><a href="about.html">About</a></li>
+					<li><a href="services.html">Services</a></li>
+					<li><a href="testimonials.html">Testimonials</a></li>
+					<li><a href="faq.html">F.A.Q.s</a></li>
+					<li><a href="gallery.html">Gallery</a></li>
+					<li><a href="timeline.html">Timeline</a></li>
+					<li class="last"><a href="404.html">404 Page</a></li>
+				</ul>
+				</li>
+				<li><a href="#">Elements</a>
+				<ul style="display: none;">
+
+					<li><a href="team.html">Team</a></li>
+					<li><a href="pricing.html">Pricing Tables</a></li>
+					<li><a href="columns.html">Columns</a></li>
+					<li><a href="rightsidebar.html">Right Sidebar</a></li>
+					<li><a href="leftsidebar.html">Left Sidebar</a></li>
+				</ul>
+				</li>
+				<li><a href="#">Portfolio</a>
+				<ul>
+					<li><a href="portfolio2.html">Two Columns</a></li>
+					<li><a href="portfolio3.html">Three Columns</a></li>
+					<li><a href="portfolio4.html">Four Columns</a></li>
+					<li><a href="portfolio5.html">Five Columns</a></li>
+
+					<li><a href="portfolio-masonry3.html">Three Masonry</a></li>
+					<li><a href="portfolio-masonry4.html">Four Masonry</a></li>
+					<li><a href="portfolio-masonry5.html">Five Masonry</a></li>
+					<li><a href="singleproject.html">Project Details</a></li>
+				</ul>
+				</li>
+				<li class="last"><a href="contact.html">Contact</a></li>
+
+			</ul>
+			</nav>
+		</div>
+	</div>
+</div>
