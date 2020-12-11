18031388c7660376ed3933b1e3389fc1d622fdc0
Leonidas P. Papadakos <papadakospan@gmail.com>
2020-12-11 20:22:14 +0200

    Menu active element

--- htdocs/topnav.php
+++ htdocs/topnav.php
@@ -3,15 +3,15 @@
 		<!--Logo-->
 		<div class="c4">
 			<a href="/">
-				<img src="images/logo.gif" class="logo" alt="Λογότυπο Υπουργείου">
+				<img src="/images/logo.gif" class="logo" alt="Λογότυπο Υπουργείου">
 			</a>
 		</div>
 		<!--Menu-->
 		<div class="c8">
 			<nav id="topNav">
 			<ul id="responsivemenu">
-				<li <?php echo 'class="active"'; ?>><a href="/"><i class="icon-home homeicon"></i></a></li>
-				<li><a href="/ministry">ΤΟ ΥΠΟΥΡΓΕΙΟ</a>
+				<li <?php if ($_SERVER["PHP_SELF"] == "/index.php") echo 'class="active"'; ?>><a href="/"><i class="icon-home homeicon"></i></a></li>
+				<li <?php if ($_SERVER["PHP_SELF"] == "/ministry/index.php") echo 'class="active"'; ?>><a href="/ministry">ΤΟ ΥΠΟΥΡΓΕΙΟ</a>
 				<ul style="display: none;">
 					<li><a href="/ministry/structure.php">Οργανωτική Δομή</a></li>
 					<li><a href="/ministry/secretariat">Γενικές Γραμματείες</a></li>
@@ -19,25 +19,25 @@
 					<li><a href="/ministry/sepe.php">Σ.ΕΠ.Ε</a></li>
 				</ul>
 				</li>
-				<li><a href="/employers">ΕΡΓΟΔΟΤΕΣ</a>
+				<li <?php if ($_SERVER["PHP_SELF"] == "/employers/index.php") echo 'class="active"'; ?>><a href="/employers">ΕΡΓΟΔΟΤΕΣ</a>
 				<ul style="display: none;">
 					<li><a href="/employers/tools">Εργαλεία</a></li>
 					<li><a href="/employers/legal">Νομοθεσία</a></li>
 				</ul>
 				</li>
-				<li><a href="/employees">ΕΡΓΑΖΟΜΕΝΟΙ</a>
+				<li <?php if ($_SERVER["PHP_SELF"] == "/employees/index.php") echo 'class="active"'; ?>><a href="/employees">ΕΡΓΑΖΟΜΕΝΟΙ</a>
 				<ul style="display: none;">
 					<li><a href="/employees/tools">Εργαλεία</a></li>
 					<li><a href="/employees/legal">Νομοθεσία</a></li>
 				</ul>
 				</li>
-				<li><a href="/unemployed">ΑΝΕΡΓΟΙ</a>
+				<li <?php if ($_SERVER["PHP_SELF"] == "/unemployed/index.php") echo 'class="active"'; ?>><a href="/unemployed">ΑΝΕΡΓΟΙ</a>
 				<ul style="display: none;">
 					<li><a href="/unemployed/tools">Εργαλεία</a></li>
 					<li><a href="/unemployed/legal">Νομοθεσία</a></li>
 				</ul>
 				</li>
-				<li><a href="/health-and-safety">ΑΣΦΑΛΕΙΑ</a>
+				<li <?php if ($_SERVER["PHP_SELF"] == "/health-and-safety/index.php") echo 'class="active"'; ?>><a href="/health-and-safety">ΑΣΦΑΛΕΙΑ</a>
 				<ul style="display: none;">
 					<li><a href="/health-and-safety/covid-19/">COVID-19</a></li>
 					<!-- TODO second ul -->
