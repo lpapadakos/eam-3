6c0f9fe15c7de11aa9bbf9a7e5fbc17453ee3983
Leonidas P. Papadakos <papadakospan@gmail.com>
2021-01-08 23:17:05 +0200

    Footer image

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -1,4 +1,4 @@
-<div id="wrapfooter">
+<footer id="wrapfooter">
 	<div class="grid">
 		<div class="row" id="footer">
 			<!-- to top button  -->
@@ -7,12 +7,6 @@
 			</p>
 			<!-- <div class="c3">
 				<img src="images/logo-footer.png" style="padding-top: 70px;" alt="">
-			</div>
-			<div class="c3">
-				<h2 class="title"><i class="icon-twitter"></i> Follow us</h2>
-				<hr class="footerstress">
-				<div id="ticker" class="query">
-				</div>
 			</div> -->
 			<!-- 1st column -->
 			<div class="c6">
@@ -27,8 +21,8 @@
 					<li><a href="#">Πολιτική Απορρήτου</a></li>
 
 				</ul>
+				<img src="/images/white_trans-14-217x45.png" style="margin-top: 40px" alt="Λογότυπο Ελληνικής Δημοκρατίας">
 			</div>
-			<!-- <div class="c3"></div> -->
 			<!-- 3rd column -->
 			<div class="c3">
 				<h2 class="title"><i class="icon-envelope-alt smallrightmargin"></i>ΕΠΙΚΟΙΝΩΝΙΑ</h2>
@@ -51,7 +45,7 @@
 			<iframe class="c3" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12579.43641904405!2d23.7310797!3d37.9804173!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x6db1fc0cbb58a00a!2zzqXPgM6_z4XPgc6zzrXOr86_IM6Vz4HOs86xz4POr86xz4IgzrrOsc65IM6azr_Ouc69z4nOvc65zrrPjs69IM6lz4DOv864zq3Pg861z4nOvQ!5e0!3m2!1sel!2sgr!4v1609353638687!5m2!1sel!2sgr" height="200" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 		</div>
 	</div>
-</div>
+</footer>
 <!-- copyright area -->
 <div class="copyright">
 	<div class="grid">
