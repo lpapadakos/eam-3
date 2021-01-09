cfd71d4b8663d5b4da24d6f1565a4558584e285d
UpTheShipCreek <sdi1700113@di.uoa.gr>
2021-01-09 19:40:27 +0200

    employees and employers covid page

---
+++ htdocs/health-and-safety/covid-19/employees.php
@@ -0,0 +1,118 @@
+<!DOCTYPE HTML>
+<htm lang="el">
+<head><meta name="viewport" content="width=device-width"/>
+<title>COVID-19 - Υπουργείο Εργασίας &amp; Κοινωνικών Υποθέσεων</title>
+<link rel="shortcut icon" href="/favicon.ico"/>
+<!-- STYLES & JQUERY
+================================================== -->
+<link rel="stylesheet" type="text/css" href="/css/style.css"/>
+<link rel="stylesheet" type="text/css" href="/css/icons.css"/>
+<link rel="stylesheet" type="text/css" href="/css/skinblue.css"/><!-- change skin color -->
+<link rel="stylesheet" type="text/css" href="/css/responsive.css"/>
+<script src="/js/jquery-1.9.0.min.js"></script><!-- the rest of the scripts at the bottom of the document -->
+</head>
+<body>
+<!-- TOP LOGO & MENU
+================================================== -->
+<?php include '../../topnav.php'; ?>
+<!-- HEADER
+================================================== -->
+<div class="undermenuarea">
+	<div class="boxedshadow">
+	</div>
+	<div class="grid">
+		<div class="row">
+			<div class="c8">
+				<h1 class="titlehead">Αντιμετώπιση πανδημίας COVID-19</h1>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- CONTENT
+================================================== -->
+<div class="grid">
+	<nav id="breadcrumbs" class="c12">
+		<p>
+			<a href="/" rel="index up up" aria-label="Αρχική" class="icon-home"></a> /
+			<a href="/health-and-safety" rel="up up">ΑΣΦΑΛΕΙΑ</a> /
+			<a href="/health-and-safety/covid-19" rel="up">COVID-19</a> /
+			<span>ΕΡΓΑΖΟΜΕΝΟΙ</span>
+		</p>
+	</nav>
+	<div class="row">
+		<!-- SIDEBAR -->
+		<div class="c3">
+			<div class="leftsidebar">
+				<nav id="sidebar-nav">
+				<h2 class="stresstitle"><a href="/health-and-safety/covid-19/index.php">COVID-19</a></h2>
+				<ul>
+					<li><a href="employees.php">Εργαζόμενοι</a></li>
+					<li><a href="employers.php">Εργοδότες</a></li>
+				</ul>
+				</nav>
+			</div>
+		</div>
+		<!-- END SIDEBAR -->
+		<!-- CONTENT -->	
+		<div class ="c9">
+			<h2 class="stresstitle">Οδηγίες για τους εργαζόμενους</h2>
+			<div class="c12">
+				<h1 class="maintitle">Γενικές οδηγίες ατομικής υγιεινής</h1>
+				<p id="bigparagraph">Κάθε εργαζόμενος έχει υποχρέωση να εφαρμόζει τους κανόνες υγείας και ασφάλειας και να φροντίζει ανάλογα με τις δυνατότητές του για την ασφάλεια και την υγεία του, 
+				καθώς και για την ασφάλεια και την υγεία των άλλων ατόμων που επηρεάζονται από τις πράξεις 
+				ή παραλείψεις του κατά την εργασίασύμφωνα με την εκπαίδευσή του και τις κατάλληλες οδηγίες του εργοδότη του. </p>
+				<h6>Το πλύσιμο και η απολύμανση των χεριών είναι ιδιαιτέρως απαραίτητα:</h6>
+				<ul class ="linelist">
+					<li>Πριν από την επαφή με τα μάτια, τη μύτη, το στόμα (φαγητό ή κάπνισμα, επίσκεψη στην τουαλέτα κ.λπ.)</li>
+					<li>Σε κάθε περίπτωση που τα χέρια μπορεί να ήλθαν σε επαφή με βιολογικά υγρά (σάλιο κ.λπ.), ακόμη κι αν χρησιμοποιήθηκαν γάντια.</li>
+				</ul>
+			</div>	
+			<div class="c12">	
+				<h6>Όταν  βήχουμε  ή  φταρνιζόμαστε,  καλύπτουμε  το  στόμα  μας  με  τον  αγκώνα 
+				 ή  ένα  χαρτομάντιλο  και  πλένουμε ή απολυμαίνουμε τα χέρια μας:</h6>
+				<ul class ="linelist">
+					<li>Απορρίπτουμε το χαρτομάντιλο, αμέσως μετά τη χρήση, σε σκεπαστό κάδο.</li>
+					<li>Αποφεύγουμε τις κοντινές επαφές ειδικά με άτομα που παρουσιάζουν συμπτώματα όπως πυρετό, βήχα, πονόλαιμο και καταρροή.</li>
+				</ul>
+			</div>
+			<div class="c12">	
+				<h6>Οι  εργαζόμενοι  που  παρουσιάζουν  
+				ένα  ή  περισσότερα  από  τα  παρακάτω  συμπτώματα  νόσου  όπως  πυρετό, βήχα, πονόλαιμο, καταρροή, θα πρέπει να τηρήσουν τις οδηγίες του ΕΟΔΥ:</h6>
+				<ul class ="linelist">
+					<li>Ενημερώνουμε τον εργοδότη ή την υπηρεσία μας</li>
+					<li>Επικοινωνούμε με το θεράποντα ιατρό μας</li>
+					<li>Επικοινωνούμε με τον ΕΟΔΥ (τηλέφωνα επικοινωνίας 210.52.12.054 ή 1135).</li>
+				</ul>
+			</div>
+			<div class="c12">
+				<h4 class="stresstitle">Απαραίτητες ενέργειες σε περίπτωση εκδήλωσης των συμπτωμάτων σε εργαζόμενο κατά τη διάρκεια εργασίας του</h4>
+				<p id="bigparagraph">Εργαζόμενος που εμφανίσει αιφνιδίως τα προαναφερόμενα συμπτώματα αναμένει σε χώρο όπου δε θα έρχεται σε επαφή με άλλα άτομα,
+				 ενημερώνονται ο ιατρός εργασίας (όπου υπάρχει) και ο προϊστάμενος όταν  δεν  υπάρχει  ιατρός  εργασίας  και  ειδοποιείται  άμεσα  
+				 ο  ΕΟΔΥ  ώστε  να  παράσχει  τις  απαραίτητες  οδηγίες, οι οποίες πρέπει να ακολουθούνται πιστά. Ο χώρος όπου παρέμεινε ο εργαζόμενος 
+				 πρέπει να καθαρίζεται και να απολυμαίνεται, μετά την αποχώρησή του, από το προσωπικό καθαριότητας, σύμφωνα με τις οδηγίες του ΕΟΔΥ 
+				 όπως αναγράφονται στην ιστοσελίδα του (www.eody.gov.gr, ενότητα: «Οδηγίες για τον περιβαλλοντικό καθαρισμό μη υγειονομικών μονάδων που έχουν
+				  εκτεθεί στον ιό SARS-CoV-2»). Σε περιπτώσεις που είναι εφικτό, συνιστάται η εξ’ αποστάσεως εργασία. </p>
+			</div>	
+		</div><!-- END CONTENT -->
+	</div><!-- end main content -->
+</div><!-- end grid -->
+
+<!-- FOOTER
+================================================== -->
+<?php include '../../footer.php' ?>
+
+<!-- JAVASCRIPTS
+================================================== -->
+<!-- all -->
+<script src="/js/modernizr-latest.js"></script>
+
+<!-- menu & scroll to top -->
+<script src="/js/common.js"></script>
+
+<!-- cycle -->
+<script src="/js/jquery.cycle.js"></script>
+
+<!-- twitter -->
+<script src="/js/jquery.tweet.js"></script>
+</body>
+</html>
