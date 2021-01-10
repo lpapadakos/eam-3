9480d12aee9d1d9c1ec75357065c2194d0b76523
Leonidas P. Papadakos <papadakospan@gmail.com>
2021-01-11 01:07:46 +0200

    Submit Success UI

--- htdocs/include/success.php
+++ htdocs/include/success.php
@@ -1,2 +1,13 @@
-<!-- TODO -->
-SUCCESS.
+<section class="row space-top text-center">
+	<i class="icon-ok-sign large" id="success"></i><br>
+	<h2>Η αίτηση σας υποβλήθηκε επιτυχώς!</h2>
+</section>
+<section class="row space-top space-bot text-center royalcontent">
+	<p>
+		Μπορείτε να αποθηκέυσετε την οριστικοποιημένη αίτηση, ή να την εκτυπώσετε:
+	</p>
+	<div id="save-area">
+		<button class="blue large"><i class="icon-save smallrightmargin"></i>Αποθήκευση</button>
+		<button class="large"><i class="icon-print smallrightmargin"></i>Εκτύπωση</button>
+	</div>
+</section>
